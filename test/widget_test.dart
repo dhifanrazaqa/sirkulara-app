e06317d8268ebe5f1971be48faaae9a5b8e37ee4
dhifanrazaqa <dhifanofficial@gmail.com>
2026-06-21 11:19:18 +0700

    init flutter

---
+++ test/widget_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('basic smoke test builds', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text('Sirkulara')),
+      ),
+    ));
+
+    expect(find.text('Sirkulara'), findsOneWidget);
+  });
+}
